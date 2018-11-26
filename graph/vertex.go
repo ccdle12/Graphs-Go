1e5455dfc66d53a976ad2f21f2baa3d17a66dcab
ccdle12 <chris.coverdale24@gmail.com>
2018-11-26 09:59:18 +0000

    graph/vertex: removing most uni-directional paths

--- graph/vertex.go
+++ graph/vertex.go
@@ -21,24 +21,26 @@ func NewVertices() []*Vertex {
 	// Create all the edges.
 	// NOTE: This graph is uni-directional.
 	A.Connections = append(A.Connections, &Edge{From: A, To: B, Fee: 4})
-	B.Connections = append(B.Connections, &Edge{From: B, To: A, Fee: 4})
+	//B.Connections = append(B.Connections, &Edge{From: B, To: A, Fee: 4})
 
 	A.Connections = append(A.Connections, &Edge{From: A, To: C, Fee: 2})
-	C.Connections = append(C.Connections, &Edge{From: C, To: A, Fee: 2})
+	//C.Connections = append(C.Connections, &Edge{From: C, To: A, Fee: 2})
 
 	B.Connections = append(B.Connections, &Edge{From: B, To: C, Fee: 1})
-	C.Connections = append(C.Connections, &Edge{From: C, To: B, Fee: 1})
+	//C.Connections = append(C.Connections, &Edge{From: C, To: B, Fee: 1})
 
 	B.Connections = append(B.Connections, &Edge{From: B, To: D, Fee: 2})
-	D.Connections = append(D.Connections, &Edge{From: D, To: B, Fee: 2})
+	//D.Connections = append(D.Connections, &Edge{From: D, To: B, Fee: 2})
 
-	C.Connections = append(C.Connections, &Edge{From: C, To: D, Fee: 4})
-	D.Connections = append(D.Connections, &Edge{From: D, To: C, Fee: 4})
+	C.Connections = append(C.Connections, &Edge{From: C, To: B, Fee: 1})
+	C.Connections = append(C.Connections, &Edge{From: C, To: D, Fee: 3})
+	//D.Connections = append(D.Connections, &Edge{From: D, To: C, Fee: 4})
 
 	C.Connections = append(C.Connections, &Edge{From: C, To: E, Fee: 5})
-	E.Connections = append(E.Connections, &Edge{From: E, To: C, Fee: 5})
+	//E.Connections = append(E.Connections, &Edge{From: E, To: C, Fee: 5})
 
 	D.Connections = append(D.Connections, &Edge{From: D, To: E, Fee: 1})
+
 	E.Connections = append(E.Connections, &Edge{From: E, To: D, Fee: 1})
 
 	// Create a slice of Vertices and return.
